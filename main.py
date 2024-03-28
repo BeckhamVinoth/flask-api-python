07082cc781c1c3391751cc7692c73e20b2d4a57e
BeckhamVinoth <vinoth.10decoders@gmail.com>
2024-03-28 23:49:37 +0530

    Hit and fetch particular temprature from given date inside csv files

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import pandas as pd
+
 
 app = Flask(__name__)
 
@@ -10,8 +12,14 @@ def home():
 
 @app.route('/api/v1/<station>/<date>')
 def about(station, date):
-    temp = 20
-    return {"station": station, "date": date, "temp": temp}
+    try:
+        file_name = "data_small/TG_STAID"+str(station).zfill(6)+".txt"
+        print(file_name)
+        df = pd.read_csv(file_name, skiprows=20, parse_dates=["    DATE"])
+        temp = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
+        return {"station": station, "date": date, "temp": temp}
+    except FileNotFoundError:
+        return str("404: File not found")
 
 
 # @app.route('/about/')
