634e70daa529ba1ee48bbadd270c083c1cc898e5
BeckhamVinoth <vinoth.10decoders@gmail.com>
2024-03-29 21:50:33 +0530

    Fetch particular date and staion details api added.

--- main.py
+++ main.py
@@ -16,27 +16,36 @@ def home():
 def about(station, date):
     try:
         file_name = "data_small/TG_STAID"+str(station).zfill(6)+".txt"
-        print(file_name)
         df = pd.read_csv(file_name, skiprows=20, parse_dates=["    DATE"])
         temp = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
         return {"station": station, "date": date, "temp": temp}
     except FileNotFoundError:
-        return str("404: File not found")
+        return str("404: Data not found")
 
 
-# @app.route('/about/')
-# def about():
-#     return render_template('about.html')
-#
-#
-# @app.route('/contact-us/')
-# def contact_us():
-#     return render_template('contact-us.html')
-#
-#
-# @app.route('/store/')
-# def store():
-#     return render_template('store.html')
+@app.route('/api/v1/<station>')
+def get_station_details(station):
+    try:
+        file_name = "data_small/TG_STAID"+str(station).zfill(6)+".txt"
+        df = pd.read_csv(file_name, skiprows=20, parse_dates=["    DATE"])
+        result = df.to_dict(orient='records')
+        return result
+    except FileNotFoundError:
+        return str("404: Station not found")
+
+
+@app.route('/api/v1/year/<station>/<year>')
+def get_year_details(station, year):
+    try:
+        file_name = "data_small/TG_STAID"+str(station).zfill(6)+".txt"
+        df = pd.read_csv(file_name, skiprows=20, parse_dates=["    DATE"])
+        df['year'] = df['    DATE'].dt.year
+        year_data = df[df['year'] == int(year)]
+        year_data.drop(columns=['year'], inplace=True)
+        result = year_data.to_dict(orient='records')
+        return result
+    except FileNotFoundError:
+        return str("404: Year records not found")
 
 
 if __name__ == '__main__':
